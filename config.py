85e0855b8b90693f8e350ce414fc92de432d42c5
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-01-14 18:56:02 +0100

    add basic errbot config

---
+++ config.py
@@ -0,0 +1,33 @@
+import logging
+
+CHATROOM_FN = 'Err'
+CHATROOM_PRESENCE = ('#xi_test_errbot',)
+BACKEND = 'IRC'
+BOT_IDENTITY = {
+    # XMPP (Jabber) mode
+    # 'username': 'err@localhost',  # The JID of the user you have created for the bot
+    # 'password': 'changeme',       # The corresponding password for this user
+    # 'server': ('host.domain.tld', 5222), # server override
+
+    ## IRC mode (Comment the others above if using this mode)
+    'nickname' : 'err-chatbot',
+    'server' : 'irc.freenode.net',
+}
+
+BOT_PREFIX = '!'
+DIVERT_TO_PRIVATE = ()
+BOT_ADMINS = ()
+
+BOT_LOG_LEVEL = logging.INFO
+BOT_DATA_DIR = 'data'
+BOT_LOG_FILE = 'data/err.log'
+
+BOT_EXTRA_PLUGIN_DIR = 'plugins'
+BOT_ASYNC = True
+AUTOINSTALL_DEPS = True
+CHATROOM_RELAY = {}
+REVERSE_CHATROOM_RELAY = {}
+
+BOT_LOG_SENTRY = False
+SENTRY_DSN = ''
+SENTRY_LOGLEVEL = BOT_LOG_LEVEL
