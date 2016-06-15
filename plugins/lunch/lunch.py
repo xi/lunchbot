66c59ac1308255eccf96030e348130ed90a51374
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-06-15 12:35:02 +0200

    Fixup

--- plugins/lunch/lunch.py
+++ plugins/lunch/lunch.py
@@ -45,7 +45,6 @@ class Lunch(BotPlugin):
     def auto_announce_lunch(self):
         now = datetime.now()
         if now.weekday() < 5 and now.hour == 12:
-            if self['last_announced'] != date.today():
             if self.get('last_announced') != date.today():
                 self.announce_lunch()
 
